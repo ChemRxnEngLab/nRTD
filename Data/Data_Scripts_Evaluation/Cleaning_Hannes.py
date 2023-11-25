829d27e472e73bb2c203ba4dc5f003b89ddd2a98
TuanaOyuncu <tuana.oyuncu@uni-ulm.de>
2023-11-25 22:56:59 +0100

    Processed data for capillary one and two

---
+++ Data/Data_Scripts_Evaluation/Cleaning_Hannes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 25 21:22:29 2023
+
+@author: tuanaoyuncu
+"""
+
+
+import numpy as np
+import scipy as sc
+x=np.load("/Users/tuanaoyuncu/Documents/GitHub/nRTD/Data/C_001/H_085_C1/S_009_C1/TOA_MGA_20231020_009_000001_x.npy")
+t=np.load("/Users/tuanaoyuncu/Documents/GitHub/nRTD/Data/C_001/H_085_C1/S_009_C1/TOA_MGA_20231020_009_000001_t.npy")
+f=sc.interpolate.interp1d(t,x[0,:])
+print(f(3))
+t_n=t[-1]
+t_b=t_n-41
+t_evel=np.linspace(t_b,t_n,500)
+t_pretty=t_evel-t_b
+x_evel=f(t_evel)
+print(x_evel)
+print(t_evel)
---
+++ Data/Data_Scripts_Evaluation/Control.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 25 21:57:07 2023
+
+@author: tuanaoyuncu
+"""
+
+import numpy as np
+x=np.load("/Users/tuanaoyuncu/Documents/GitHub/nRTD/Data/C_001/H_085_C1/S_009_C1/TOA_MGA_20231020_009_000001_t_processed.npy")
+print(x)
