ec2dda575730fc03e1092b2150dcbdbb3693e0eb
TuanaOyuncu <tuana.oyuncu@uni-ulm.de>
2023-11-27 11:58:36 +0100

    data examination

---
+++ Data/Data_Scripts_Evaluation/Experiment_1/H_135_C1_S_010/Cleaning_and_Plots_H_135_C1_S_010.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 25 21:25:12 2023
+
+@author: tuanaoyuncu
+"""
+
+import numpy as np
+import scipy as sc
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+WD = Path("/Users/tuanaoyuncu/Documents/GitHub/nRTD")
+DATA = WD / "Data"
+PWD=DATA / "C_001" / "H_135_C1" / "S_010_C1" 
+
+def process_data(file_num):
+    # Construct file paths for _x.npy and _t.npy
+    x_file_path = PWD / f"TOA_MGA_20231020_010_{file_num:06d}_x.npy"
+    t_file_path = PWD / f"TOA_MGA_20231020_010_{file_num:06d}_t.npy"
+
+    # Load _x.npy and _t.npy
+    x = np.load(x_file_path)
+    t = np.load(t_file_path)
+
+    # Interpolate data
+    f = sc.interpolate.interp1d(t, x[0, :])
+    t_n = t[-1]
+    t_b = t_n - 41
+    t_evel = np.linspace(t_b, t_n, 500)
+    t_pretty = t_evel - t_b
+    x_evel = f(t_evel)
+
+    # Save processed data
+    np.save(PWD / f"TOA_MGA_20231020_010_{file_num:06d}_t_processed.npy", t_pretty)
+    np.save(PWD / f"TOA_MGA_20231020_010_{file_num:06d}_x_processed.npy", x_evel)
+
+    # Plot results
+    plt.plot(t_pretty, x_evel, label=file_num)
+    #plt.vlines(6, 0,0.05)
+
+def main():
+    # Specify the range of file numbers you want to process
+    start_file_num = 1
+    end_file_num = 20
+
+    # Iterate over the file numbers and process each data file
+    for file_num in range(start_file_num, end_file_num + 1):
+    #for file_num in [9,19]:
+        process_data(file_num)
+
+    # Show the legend and plot
+    plt.legend()
+    plt.xlabel("t/s")
+    plt.ylabel("x/1")
+    #plt.savefig("s_014.png")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
