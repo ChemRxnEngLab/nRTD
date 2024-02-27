8bdef4ddf05ea24a14237ba43d74ef98d7d208af
TuanaOyuncu <tuana.oyuncu@uni-ulm.de>
2024-02-27 01:17:10 +0100

    learning rate investigation

--- Experiments/Preliminary/Convolution_Script_Parameter_Variation/Convolution_Training_006/TOA_MGA_2024_Convolution_Training_006.py
+++ Experiments/Preliminary/Convolution_Script_Parameter_Variation/Convolution_Training_006/TOA_MGA_2024_Convolution_Training_006.py
@@ -1,70 +1,3 @@
-# import sys
-# sys.path.append("/Users/tuanaoyuncu/Documents/GitHub/nRTD/lib")
-# import torch
-# from torch.utils.data import TensorDataset, DataLoader
-# import lightning.pytorch as pl
-# import matplotlib.pyplot as plt
-# from nrtd import RTDModule
-# import numpy as np
-
-
-# n_disc = 377
-# t_input = torch.linspace(0, 31, n_disc)
-# c_in = torch.zeros((500, 1, n_disc))
-# c_in[:, :, t_input > 1] = 0.05
-# t_conv = torch.tensor(np.load("/Users/tuanaoyuncu/Documents/GitHub/nRTD/Data/C_001/H_085_C1/S_009_C1/TOA_MGA_20231020_009_000001_t_processed.npy"), dtype=torch.float32)
-# c_out = torch.tensor(np.load("/Users/tuanaoyuncu/Documents/GitHub/nRTD/Data/C_001/H_085_C1/S_009_C1/TOA_MGA_20231020_009_000001_x_processed.npy"), dtype=torch.float32)
-# c_out = c_out.unsqueeze(0).expand(500, -1, -1)
-# t_conv = t_conv.unsqueeze(0).expand(500, -1)
-
-# plt.show()
-# print(f"c_in size: {c_in.size()}")
-# print(f"c_out size: {c_out.size()}")
-# print(f"t_conv size: {t_conv.size()}")
-
-# model = RTDModule(
-#     kernel_size=122,
-#     learning_rate=1e-3,
-# )
-# c_conv = model(c_in)
-# E = model.net.E[0]
-# t_E = torch.linspace(0, 10, model.kernel_size)
-
-# plt.figure()
-# plt.plot(t_input, c_in[0, 0, :].numpy(), label="SF", color="blue")
-# plt.plot(t_conv[0, :].numpy(), c_out[0, 0, :].numpy(), label="Exp", color="green")
-# plt.plot(t_conv[0, :].numpy(), c_conv[0, 0, :].detach().numpy(), label="Predicted", color="red")
-# plt.plot(t_E, E, label="E", color="orange")
-# plt.legend()
-# plt.xlim((0, 10))
-# plt.ylim((0, 0.2))
-# plt.show()
-
-# ds = TensorDataset(c_in, c_out)
-# dl = DataLoader(ds, batch_size=1)
-
-# trainer = pl.Trainer(
-#     accelerator="auto",
-#     max_epochs=100,
-# )
-# trainer.fit(model, dl)
-
-# c_conv = model(c_in)
-# E = model.net.E[0]
-# t_E = torch.linspace(0, 10, model.kernel_size)
-
-# plt.figure()
-# plt.plot(t_input, c_in[0, 0, :].numpy(), label="SF", color="blue")
-# plt.plot(t_conv[0, :].numpy(), c_out[0, 0, :].numpy(), label="Exp", color="green")
-# plt.plot(t_conv[0, :].numpy(), c_conv[0, 0, :].detach().numpy(), label="Predicted", color="red")
-# plt.plot(t_E, E, label="E", color="orange")
-# plt.xlim((0, 10))
-# plt.ylim((0, 0.2))
-# plt.legend()
-# plt.show()
-
-##########
-
 import sys
 
 sys.path.append("/Users/tuanaoyuncu/Documents/GitHub/nRTD/lib")
@@ -103,8 +36,6 @@ for i, file_num in enumerate(file_numbers):
     t_conv_list.append(t_conv)
 c_out = torch.cat(c_out_list, dim=0)
 t_conv = torch.cat(t_conv_list, dim=0)
-# c_out = c_out.unsqueeze(2).expand(-1, -1, 1, -1)
-# t_conv = t_conv.unsqueeze(2).expand(-1, -1, 1, -1)
 plt.show()
 print(f"c_in size: {c_in.size()}")
 print(f"c_out size: {c_out.size()}")
@@ -143,14 +74,15 @@ plt.ylim((0, 0.2))
 plt.show()
 
 print(model(c_in).size())
-# exit()
 
 ds = TensorDataset(c_in, c_out)
-# split the dataset into train and test
-train_ds, test_ds = random_split(ds, [0.8, 0.2])
+# split the dataset into train and test 
+#train_ds, test_ds = random_split(ds, [0.8, 0.2])
 
-train_dl = DataLoader(train_ds, batch_size=1)
-test_dl = DataLoader(test_ds, batch_size=1, shuffle=True)
+# train_dl = DataLoader(train_ds, batch_size=1)
+# test_dl = DataLoader(test_ds, batch_size=1, shuffle=True)
+
+dl = DataLoader(ds, batch_size=1, shuffle=False)
 
 # set up the logger
 wandb_logger = pl_loggers.WandbLogger(
@@ -164,9 +96,11 @@ trainer = pl.Trainer(
     logger=wandb_logger,
 )
 
-trainer.fit(model, train_dl)
+#trainer.fit(model, train_dl)
+trainer.fit(model, dl)
 # adds an epoch at the end to calculate the final loss at the traineing end
-trainer.test(model, test_dl)
+#trainer.test(model, test_dl)
+trainer.test(model, dl)### check it maybe you will see changes??
 wandb.finish()
 
 ##################
@@ -198,5 +132,8 @@ plt.plot(
 plt.plot(t_E, E, label="E", color="orange")
 plt.xlim((0, 10))
 plt.ylim((0, 0.2))
+plt.savefig("Figure_006_002_False")
 plt.legend()
 plt.show()
+
+
