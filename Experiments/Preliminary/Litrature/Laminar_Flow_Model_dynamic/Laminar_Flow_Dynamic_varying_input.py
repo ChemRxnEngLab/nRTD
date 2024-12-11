9c8a28cd62f701cc1f4a808092973c7a1060f917
TuanaOyuncu <tuana.oyuncu@uni-ulm.de>
2024-12-11 01:33:13 +0100

    Dynamic

--- Experiments/Preliminary/Litrature/Laminar_Flow_Model_dynamic/Laminar_Flow_Dynamic_varying_input.py
+++ Experiments/Preliminary/Litrature/Laminar_Flow_Model_dynamic/Laminar_Flow_Dynamic_varying_input.py
@@ -4,34 +4,26 @@ import numpy.typing as npt
 import os
 from scipy.interpolate import interp1d
 
-# Parameters
 noise_level = 0.001
 tau = 5.0
 base_dir = '/Users/tuanaoyuncu/Documents/GitHub/nRTD/Experiments/Preliminary/Litrature/Laminar_Flow_Model_dynamic'
-i_values = np.linspace(0, 1, 100) 
+i_values = np.linspace(0, 10, 100) 
 
-# Laminar flow function
 def laminarflow(t: npt.NDArray[np.float64], tau: float) -> npt.NDArray[np.float64]:
     E = np.zeros_like(t)
     E[t >= tau / 2] = (tau**2) / (2 * (t[t >= tau / 2]**3))
     return E
 
-
-# Time vector
 t = np.linspace(0, 300, 500)
 t_75 = np.linspace(0, 150, 250)
-
-# Generate laminar flow E
 E = laminarflow(t, tau)
 E_normalized = E / np.sum(E)
 
-# Directory for saving results
 tau_dir = os.path.join(base_dir, f'tau_{tau}_with_varying_step')
 os.makedirs(tau_dir, exist_ok=True)
 
 # Loop over i values
 for i in i_values:
-    # Define the input function with i-dependent step
     def input_function(t: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
         return 0.5 * (np.sin(t - (np.pi / 2)+i) + 1)  # Input depends on i
     
@@ -47,8 +39,6 @@ for i in i_values:
     valid_indices = t_conv_full <= t[-1]
     t_conv = t_conv_full[valid_indices]
     c_out = c_out_full[valid_indices]
-
-    # Add noise
     variance_scale = noise_level * np.var(c_out)
     noise = np.random.normal(0, np.sqrt(variance_scale), c_out.shape)
     c_out_noisy = c_out + noise
@@ -59,20 +49,15 @@ for i in i_values:
     np.save(os.path.join(save_dir, f'time.npy'), t_conv)
     np.save(os.path.join(save_dir, f'concentration.npy'), c_out_noisy)
     np.save(os.path.join(save_dir, f'input_function_interp.npy'), c_0_75)
-    print(c_0_75)
-    print(c_out_noisy.shape)
-    # # Plot results for each i
+    # print(c_0_75)
+    # print(c_out_noisy.shape)
     # fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
-
-    # # Top plot: Laminar Flow E
     # ax1.plot(t, E / E.max(), label=f'Laminar Flow E (tau = {tau})', color="red")
     # ax1.set_ylim(0, 1.1)
     # ax1.set_xlim(0, 50)
     # ax1.set_xlabel('Time')
     # ax1.set_ylabel('E')
     # ax1.legend()
-
-    # # Bottom plot: Convolved Output
     # ax2.plot(t_conv, c_out, label='Convolved Output c_out', color="blue")
     # ax2.plot(t_conv, c_out_noisy, label='Convolved Output (Noisy) c_out', linestyle='--', color="red")
     # ax2.plot(t, input_func, label='Input Function', linestyle='--', color='black')
@@ -80,9 +65,6 @@ for i in i_values:
     # ax2.set_xlim(0, 50)
     # ax2.set_xlabel('Time')
     # ax2.set_ylabel('Concentration')
-    # ax2.legend()
-
-    # # Show and save the plot
-    # plt.suptitle(f'Convolution Results for i = {i:.1f}')
+    # ax2.legend()    
     # plt.savefig(os.path.join(tau_dir, f'plot_i_{i:.1f}.png'))
     # plt.show()
