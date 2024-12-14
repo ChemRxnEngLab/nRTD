35fdc10878081c034bc7d21a836baf47f44ec566
TuanaOyuncu <tuana.oyuncu@uni-ulm.de>
2024-12-14 16:33:40 +0100

    dispersion model

--- Experiments/Preliminary/Convolution_script/Convolution_022_Dispersion_Model/Convolution_022_Dispersion_Model_bo_1.py
+++ Experiments/Preliminary/Convolution_script/Convolution_022_Dispersion_Model/Convolution_022_Dispersion_Model_bo_1.py
@@ -33,7 +33,7 @@ sys.path.append(module_path)
 tau_5_dir =r'D:\Tuana\nRTD\Experiments\Preliminary\Litrature\Dispersion_Model\Bo1_200disc_240s'
 #tau_5_dir = '/Users/tuanaoyuncu/Documents/GitHub/nRTD/Experiments/Preliminary/Litrature/Dispersion_Model/Bo3_200disc_140s'
 
-epoch=1
+epoch=30000
 n_in_1, n_out_1, n_e_1, = sp.symbols(
     "n_in_1 n_out_1 n_e_1 ", positive=True, real=True
 )
