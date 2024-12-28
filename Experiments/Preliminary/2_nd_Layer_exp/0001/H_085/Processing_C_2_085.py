13da12f1ca88ed829272b0c072abcb6d5089febd
TuanaOyuncu <tuana.oyuncu@uni-ulm.de>
2024-12-28 17:38:43 +0100

    275

--- Experiments/Preliminary/2_nd_Layer_exp/0001/H_085/Processing_C_2_085.py
+++ Experiments/Preliminary/2_nd_Layer_exp/0001/H_085/Processing_C_2_085.py
@@ -21,7 +21,7 @@ def process_data(file_num):
     f = sc.interpolate.interp1d(t, x[0, :])
     t_n = t[-1]
     t_b = t_n - 51
-    t_evel = np.linspace(t_b, t_n, 442)
+    t_evel = np.linspace(t_b, t_n, 275)
     t_pretty = t_evel - t_b
     x_evel_1 = f(t_evel)
     x_evel_2=x_evel_1/224
@@ -29,8 +29,8 @@ def process_data(file_num):
     x_evel=x_evel_2
 
     # Save processed data
-    np.save(PWD / f"TOA_MGA_20231020_012_{file_num:06d}_t_processed_conc_442.npy", t_pretty)
-    np.save(PWD / f"TOA_MGA_20231020_012_{file_num:06d}_x_processed_conc_442.npy", x_evel)
+    np.save(PWD / f"TOA_MGA_20231020_012_{file_num:06d}_t_processed_conc_275.npy", t_pretty)
+    np.save(PWD / f"TOA_MGA_20231020_012_{file_num:06d}_x_processed_conc_275.npy", x_evel)
 
     # Plot results
     plt.plot(t_pretty, x_evel, label=file_num)
