0f603fe160b5b486b40b80a37698504df2df914f
TuanaOyuncu <tuana.oyuncu@uni-ulm.de>
2024-12-29 02:43:24 +0100

    150 flowrate

--- Experiments/Preliminary/2_nd_Layer_exp/0001/H_135/Processing_135_C2.py
+++ Experiments/Preliminary/2_nd_Layer_exp/0001/H_135/Processing_135_C2.py
@@ -23,14 +23,14 @@ def process_data(file_num):
     t_evel = np.linspace(t_b, t_n, 377)
     t_pretty = t_evel - t_b
     x_evel_1 = f(t_evel)
-    x_evel_2=x_evel_1/224
+    x_evel_2=(x_evel_1/22.4)*1000
     x_evel_2/= x_evel_2.max()
     x_evel=x_evel_2
     #print(x_evel)
 
     # Save processed data
-    np.save(PWD / f"TOA_MGA_20231020_013_{file_num:06d}_t_processed_conc_377.npy", t_pretty)
-    np.save(PWD / f"TOA_MGA_20231020_013_{file_num:06d}_x_processed_conc_377.npy", x_evel)
+    np.save(PWD / f"TOA_MGA_20231020_013_{file_num:06d}_t_processed_conc_norm.npy", t_pretty)
+    np.save(PWD / f"TOA_MGA_20231020_013_{file_num:06d}_x_processed_conc_norm.npy", x_evel)
 
     # Plot results
     plt.plot(t_pretty, x_evel, label=file_num)
